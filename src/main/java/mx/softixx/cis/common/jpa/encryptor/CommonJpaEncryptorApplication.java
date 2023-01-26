ec0a12693f082ff41716b2b9bf221ded0ca08fe3
Maikel Guerra Ferrer <mguerraferrer@gmail.com>
2023-01-26 15:31:16 -0600

    Initial commit

---
+++ src/main/java/mx/softixx/cis/common/jpa/encryptor/CommonJpaEncryptorApplication.java
@@ -0,0 +1,7 @@
+package mx.softixx.cis.common.jpa.encryptor;
+
+public class CommonJpaEncryptorApplication {
+	public static void main(String[] args) {
+		// Main
+	}
+}
---
+++ src/main/java/mx/softixx/cis/common/jpa/encryptor/converter/AttributeEncryptor.java
@@ -0,0 +1,48 @@
+package mx.softixx.cis.common.jpa.encryptor.converter;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.spec.SecretKeySpec;
+import jakarta.persistence.AttributeConverter;
+import java.security.InvalidKeyException;
+import java.security.Key;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
+
+@Component
+public class AttributeEncryptor implements AttributeConverter<String, String> {
+
+    private final Key key;
+    private final Cipher cipher;
+
+    public AttributeEncryptor(@Value("${encryptor.key}") String secretKey) throws NoSuchAlgorithmException, NoSuchPaddingException  {
+        key = new SecretKeySpec(secretKey.getBytes(), "AES");
+        cipher = Cipher.getInstance("AES");
+    }
+
+    @Override
+    public String convertToDatabaseColumn(String attribute) {
+        try {
+            cipher.init(Cipher.ENCRYPT_MODE, key);
+            return Base64.getEncoder().encodeToString(cipher.doFinal(attribute.getBytes()));
+        } catch (IllegalBlockSizeException | BadPaddingException | InvalidKeyException e) {
+            throw new IllegalStateException(e);
+        }
+    }
+
+    @Override
+    public String convertToEntityAttribute(String dbData) {
+        try {
+            cipher.init(Cipher.DECRYPT_MODE, key);
+            return new String(cipher.doFinal(Base64.getDecoder().decode(dbData)));
+        } catch (InvalidKeyException | BadPaddingException | IllegalBlockSizeException e) {
+            throw new IllegalStateException(e);
+        }
+    }
+    
+}
